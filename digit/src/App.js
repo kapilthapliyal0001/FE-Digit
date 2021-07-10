9b8769fa5665c4de43214d1dc70d604fd49fb7aa
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2021-07-10 14:52:49 +0200

    Update App.js

--- digit/src/App.js
+++ digit/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom"
+import {BrowserRouter as Router, Route} from "react-router-dom"
 // Components Imports
 import MainPage from './components/MainPage';
 
