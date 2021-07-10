d501f8d5b217162c547c9f395efc80080a63c2bd
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2021-07-10 16:51:08 +0200

    finally hover working

--- digit/src/components/SideBar.jsx
+++ digit/src/components/SideBar.jsx
@@ -5,7 +5,7 @@ import { Container, Image} from 'react-bootstrap';
 export default class SideBar extends Component {
     render() {
         return (
-            <Container lg={12} md={12} sm={12}  className = "my-3" onClick={() => {
+            <Container lg={12} md={12} sm={12}  className = "my-3 ease-in-out" onClick={() => {
                 this.props.selectedUrl(this.props.element.src.large)   // state lifting props on click
             }}>
                 <Image className="img-thumbnail" src={this.props.element.src.small} alt="" fluid/>
